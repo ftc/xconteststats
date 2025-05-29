072867f7e5407e01f96a11b86f596826ced82139
s <s@Surface7>
2025-05-29 17:50:26 -0600

    fix counting bug

--- analyzeFlights.py
+++ analyzeFlights.py
@@ -1,17 +1,32 @@
 import json
 import argparse
 
-def getFlyingDays(files):
-    daySet = set()
+def forEachFlight(files, functionToApply):
     for file in files:
         f = open(file,'r')
         file_lines = f.read()
         parsed = json.loads(file_lines)
         for flight in parsed['items']:
-            time_claim = flight['timeClaim']
-            date_string = time_claim.split('T')[0]
-            daySet.add(date_string)
-        return len(daySet)
+            functionToApply(flight)
+
+def getFlyingDays(files):
+    daySet = set()
+
+    def toApply(flight):
+        time_claim = flight['timeClaim']
+        date_string = time_claim.split('T')[0]
+        daySet.add(date_string)
+    forEachFlight(files,toApply)
+    return len(daySet)
+
+def getFlightsAtEachSite(files):
+    for file in files:
+        f = open(file,'r')
+        file_lines = f.read()
+        parsed = json.loads(file_lines)
+        for flight in parsed['items']:
+            print()
+
 
 def main():
     parser = argparse.ArgumentParser(description="A utility to extract statistics from XContest " \
@@ -24,4 +39,6 @@ def main():
     print(f"Input file: {args.input_file}")
     print(f"Flying days: {getFlyingDays(args.input_file.split(' '))}")
 if __name__ == "__main__":
+    # print(getFlightsAtEachSite(["flights.ShawnKM.world2025.json","flights.ShawnKM.world2024.json"]))
+    # print(getFlyingDays(["flights.ShawnKM.world2025.json","flights.ShawnKM.world2024.json"]))
     main()
